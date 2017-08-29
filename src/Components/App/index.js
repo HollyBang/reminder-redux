5bbabde35619790cdc204abf55bea3bda4faac05
Andrey Bar <mazafakand@gmail.com>
2017-08-29 16:29:46 +0300

    prepeared first component

---
+++ src/Components/App/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+        Title
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
