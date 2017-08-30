bcb6564ae7e389c81a0c421a2fdc2025fa2672a5
Andrey Bar <mazafakand@gmail.com>
2017-08-30 11:53:46 +0300

    material ui working now

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 
-import React, { Component } from 'react';
+
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class App extends Component {
+
+  
   render() {
+    const style = {
+      margin: 12,
+    };
     return (
       <div className="App">
         <div className="title">
-        Title
+          Title
         </div>
-        
+        <div className="form">
+          <TextField
+            hintText="Hint Text"
+            floatingLabelText="Floating Label Text"
+          />
+          <RaisedButton label="Primary" primary={true} style={style} />
+        </div>
       </div>
     );
   }
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import App from './Components/App';
+
+const MyApp = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
 ReactDOM. render(
-  <div>Hello world </div>, document.getElementById('root')
+  <MyApp />, document.getElementById('root')
 );
